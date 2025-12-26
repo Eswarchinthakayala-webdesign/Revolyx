455c23b11a97516c27eb9055e47fc93359d43801
Eswar <eswarch2004y@gmail.com>
2025-12-26 23:05:12 +0530

    HK

--- src/pages/SVGViewerPage.jsx
+++ src/pages/SVGViewerPage.jsx
@@ -556,7 +556,7 @@ const normalizeSVG = (svg) =>
 
 
         {/* RIGHT PANEL */}
-        <aside className="sm:w-80 border-l border-zinc-200 dark:border-zinc-800 flex flex-col shrink-0 overflow-y-auto bg-zinc-50/30 dark:bg-zinc-950">
+        {/* <aside className="sm:w-80 border-l border-zinc-200 dark:border-zinc-800 flex flex-col shrink-0 overflow-y-auto bg-zinc-50/30 dark:bg-zinc-950">
           <div className="p-4 border-b border-zinc-200 dark:border-zinc-800 flex items-center justify-between">
             <span className="text-[10px] font-bold uppercase tracking-widest text-zinc-500">Preview</span>
             
@@ -590,7 +590,6 @@ const normalizeSVG = (svg) =>
             </Dialog>
           </div>
 
-          {/* MAIN PREVIEW AREA */}
           <div className="p-4 space-y-6">
             <div 
               className="aspect-square rounded-xl border border-zinc-200 dark:border-zinc-800 relative overflow-hidden flex items-center justify-center group bg-white dark:bg-zinc-900 shadow-xl"
@@ -607,7 +606,7 @@ const normalizeSVG = (svg) =>
               />
             </div>
             
-            {/* TRANSFORM TOOLS */}
+         
             <div className="space-y-4">
               <div className="flex items-center justify-between">
                  <span className="text-[10px] font-bold uppercase text-zinc-400">Transforms</span>
@@ -621,7 +620,7 @@ const normalizeSVG = (svg) =>
                  </div>
               </div>
             </div>
-            {/* SIZE CONTROL SLIDER */}
+           
             <div className="space-y-3 px-1">
                <div className="flex justify-between items-center">
                  <span className="text-[10px] font-bold text-zinc-500 uppercase">Zoom Scale</span>
@@ -690,7 +689,7 @@ const normalizeSVG = (svg) =>
               Copy SVG Code
             </Button>
           </div>
-        </aside>
+        </aside> */}
       </main>
     </div>
   );
